13697f1554de5bd36211194dafa49f71fd77158e
kongou1121 <kongou1121.2002@gmail.com>
2024-02-22 06:52:10 +0900

    kadai_007ファイルを追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>課題007</title>
+</head>
+<body>
+  <?php
+  // 連想配列を作成
+  $onion_date = ['name' => 'onion', 'price' => 200, 'weight' => 160,];
+
+  // 値を出力する
+  print_r($onion_date);
+
+  ?>
+</body>
